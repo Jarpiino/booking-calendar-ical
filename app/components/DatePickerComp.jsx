d6a99676657bd30f380a90cd6091af03dc93b9df
Jarno Ojasaar <jarno.ojasaar@gmail.com>
2023-06-02 16:01:30 +0300

    refactored code

--- app/components/DatePickerComp.jsx
+++ app/components/DatePickerComp.jsx
@@ -6,29 +6,37 @@ import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
 import { addDays, subDays } from "date-fns";
 
+// classes for datepickers
+const classes = "px-6 py-4 rounded-full ";
+
 export default function DatePickerComp() {
+  // set defualt date as today and tomorrow
+  let defaultDate = new Date();
+  // set defualt date as today
+  const today = new Date();
+  // set default date + 1 as tomorrow
+  const tomorrow = defaultDate.setDate(defaultDate.getDate() + 1);
+  // set default state for calendar
+  const [startDate, setStartDate] = useState(today);
+  const [endDate, setEndDate] = useState(tomorrow);
+
+  // fetch data from backend
   const fetcher = (url) => fetch(url).then((r) => r.json());
   const { data, error, isLoading } = useSWR(
     "http://localhost:3000/api/hello",
     fetcher
   );
-
-  let datee = new Date();
-  const today = new Date();
-  const tomorrow = datee.setDate(datee.getDate() + 1);
-  const [startDate, setStartDate] = useState(today);
-  const [endDate, setEndDate] = useState(tomorrow);
-  const classes = "px-6 py-4 rounded-full ";
   if (error) return <div className="text-5xl">failed to load</div>;
   if (isLoading) return <div className="text-5xl">loading...</div>;
 
+  // alter the response data
   let dates = Object.entries(data);
   dates = dates[0][1];
   dates = Object.entries(dates);
   dates = Object.entries(dates);
 
-  let datesArray = [];
   // make and array of dates
+  let datesArray = [];
   dates.forEach(([key, value]) => {
     if (value[1].start === undefined) {
       return;
@@ -43,7 +51,6 @@ export default function DatePickerComp() {
   });
   // format array for date-picker
   let datesArrayNew = [];
-  console.log(datesArray);
   datesArray.map(function (item, i) {
     return datesArrayNew.push([
       { start: subDays(item[0], 0), end: addDays(item[1], -1) },
@@ -56,6 +63,10 @@ export default function DatePickerComp() {
       return datesArrayBEST.push(item[0]);
     });
   };
+  datesArrayBEST.push({
+    start: subDays(new Date(), 5000),
+    end: addDays(new Date(), -1),
+  });
   finalArray();
   return (
     <div className="flex gap-4 text-xl font-medium text-black">
@@ -67,6 +78,7 @@ export default function DatePickerComp() {
         startDate={startDate}
         excludeDateIntervals={datesArrayBEST}
         endDate={endDate}
+        monthsShown={2}
         // inline
       />
       <DatePicker
